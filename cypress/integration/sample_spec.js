7089f9e053cef7f6d22b58e8ba7af24bda9e6716
gpeterso <gpeterso@umn.edu>
2019-08-08 15:46:33 -0500

    Reformat

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -3,7 +3,10 @@ import urls from '../support/urls';
 describe('Search Test', () => {
   it('works', () => {
     cy.visit(urls.searchPage);
-    cy.get('#searchBar').type('test{enter}');
-    cy.get('div.results-container').children().should('have.length.greaterThan', 0);
+    cy.get('#searchBar')
+      .type('test{enter}');
+    cy.get('div.results-container')
+      .children()
+      .should('have.length.greaterThan', 0);
   });
 });
