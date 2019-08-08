d98983562c52e84ce9fdf2c1e1ff11e1d58ae168
gpeterso <gpeterso@umn.edu>
2019-08-08 14:16:36 -0500

    Add basic search assertion

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -3,5 +3,7 @@ import urls from '../support/urls';
 describe('Search Test', () => {
   it('works', () => {
     cy.visit(urls.searchPage);
+    cy.get('#searchBar').type('test{enter}');
+    cy.get('div.results-container').children().should('have.length.greaterThan', 0);
   });
 });
